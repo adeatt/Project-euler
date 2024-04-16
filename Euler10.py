87d15b89769efd7eceddc8541b99302a03ceccff
adeatt <146625599+adeatt@users.noreply.github.com>
2024-04-16 21:18:02 +0200

    Add files via upload

---
+++ Euler10.py
@@ -0,0 +1,14 @@
+primelist = [2,]
+
+
+
+for num in range(3, 2000001, 2):
+
+    for n in range(0, len(primelist)-1):
+        if num % primelist[n] == 0:
+            break
+    else:
+        primelist.append(num)       
+
+
+print(sum(primelist))
